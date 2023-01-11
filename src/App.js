9c9f3866cd25a926f9adab6e27614b3cd4964839
unaivan22 <unaivan22@gmail.com>
2023-01-11 23:17:18 +0700

    delete toast

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import RouterPage from './pages/RouterPage';
 
 function App() {
+  console.error = (function() {
+    var error = console.error
+
+    return function(exception) {
+        if ((exception + '').indexOf('Warning: A component is `contentEditable`') != 0) {
+            error.apply(console, arguments)
+        }
+    }
+  })()
   return (
     <RouterPage />
   );
