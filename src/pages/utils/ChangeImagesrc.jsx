a4e0562503b10396832a8ff49f84062a6b172f10
unaivan <unaivan@gmail.com>
2023-01-12 08:24:49 +0700

    fixing async

--- src/pages/utils/ChangeImagesrc.jsx
+++ src/pages/utils/ChangeImagesrc.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function ChangeImagesrc() {
-window.onload=function(){
+window.onload = async () => {
     document.getElementById('imageUpload').addEventListener('change', readURL, true);
     function readURL(){
         const file = document.getElementById("imageUpload").files[0];
